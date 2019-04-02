9c13eb87c74ab73c007921c85bafe45f54cdb5a2
hhk24 <mark@jiaozhentech.com>
2019-04-02 09:50:46 +0800

    avl树完整

--- mk_AVLTree/src/main/java/avl_tree_inter/AVLTree.java
+++ mk_AVLTree/src/main/java/avl_tree_inter/AVLTree.java
@@ -93,6 +93,8 @@ public class AVLTree<K extends Comparable<K>, V> {
 		root = add(root, key, value);
 	}
 
+	// 向以node为根的二分搜索树中插入元素(key, value)，递归算法
+	// 返回插入新节点后二分搜索树的根
 	private Node add(Node node, K key, V value) {
 
 		if (node == null) {
@@ -113,8 +115,22 @@ public class AVLTree<K extends Comparable<K>, V> {
 
 		// 计算平衡因子
 		int balanceFactor = getBalanceFactor(node);
-		if (Math.abs(balanceFactor) > 1)
-			System.out.println("unbalanced : " + balanceFactor);
+
+		// 平衡维护
+		if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0)
+			return rightRotate(node);
+
+		if (balanceFactor < -1 && getBalanceFactor(node.right) <= 0)
+			return leftRotate(node);
+
+		if (balanceFactor > 1 && getBalanceFactor(node.left) < 0) {
+			node.left = leftRotate(node.left);
+			return rightRotate(node);
+		}
+		if (balanceFactor < -1 && getBalanceFactor(node.right) > 0) {
+			node.right = rightRotate(node.right);
+			return leftRotate(node);
+		}
 
 		return node;
 	}
@@ -189,12 +205,13 @@ public class AVLTree<K extends Comparable<K>, V> {
 		if (node == null)
 			return null;
 
+		Node retNode;
 		if (key.compareTo(node.key) < 0) {
 			node.left = remove(node.left, key);
-			return node;
+			retNode = node;
 		} else if (key.compareTo(node.key) > 0) {
 			node.right = remove(node.right, key);
-			return node;
+			retNode = node;
 		} else { // key.compareTo(node.key) == 0
 
 			// 待删除节点左子树为空的情况
@@ -202,7 +219,7 @@ public class AVLTree<K extends Comparable<K>, V> {
 				Node rightNode = node.right;
 				node.right = null;
 				size--;
-				return rightNode;
+				retNode = rightNode;
 			}
 
 			// 待删除节点右子树为空的情况
@@ -210,21 +227,100 @@ public class AVLTree<K extends Comparable<K>, V> {
 				Node leftNode = node.left;
 				node.left = null;
 				size--;
-				return leftNode;
+				retNode = leftNode;
 			}
 
 			// 待删除节点左右子树均不为空的情况
+			else {
+				// 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
+				// 用这个节点顶替待删除节点的位置
+				Node successor = minimum(node.right);
+				// successor.right = removeMin(node.right);
+				successor.right = remove(node.right, successor.key);
+				successor.left = node.left;
 
-			// 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
-			// 用这个节点顶替待删除节点的位置
-			Node successor = minimum(node.right);
-			successor.right = removeMin(node.right);
-			successor.left = node.left;
+				node.left = node.right = null;
 
-			node.left = node.right = null;
-
-			return successor;
+				// return successor;
+				retNode = successor;
+			}
 		}
+		if (retNode == null)
+			return null;
+
+		// 更新height
+		retNode.height = 1 + Math.max(getHeight(retNode.left), getHeight(retNode.right));
+
+		// 计算平衡因子
+		int balanceFactor = getBalanceFactor(retNode);
+
+		// 平衡维护
+		// LL
+		if (balanceFactor > 1 && getBalanceFactor(retNode.left) >= 0)
+			return rightRotate(retNode);
+
+		// RR
+		if (balanceFactor < -1 && getBalanceFactor(retNode.right) <= 0)
+			return leftRotate(retNode);
+
+		// LR
+		if (balanceFactor > 1 && getBalanceFactor(retNode.left) < 0) {
+			retNode.left = leftRotate(retNode.left);
+			return rightRotate(retNode);
+		}
+
+		// RL
+		if (balanceFactor < -1 && getBalanceFactor(retNode.right) > 0) {
+			retNode.right = rightRotate(retNode.right);
+			return leftRotate(retNode);
+		}
+
+		return retNode;
+
+	}
+
+	// 对节点y进行向右旋转操作，返回旋转后新的根节点x
+	// y x
+	// / \ / \
+	// x T4 向右旋转 (y) z y
+	// / \ - - - - - - - -> / \ / \
+	// z T3 T1 T2 T3 T4
+	// / \
+	// T1 T2
+	private Node rightRotate(Node y) {
+
+		Node x = y.left;
+		Node T3 = x.right;
+		x.right = y;
+		y.left = T3;
+
+		// 更新height
+		y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+		x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+
+		return x;
+	}
+
+	// 对节点y进行向左旋转操作，返回旋转后新的根节点x
+	// y x
+	// / \ / \
+	// T1 x 向左旋转 (y) y z
+	// / \ - - - - - - - -> / \ / \
+	// T2 z T1 T2 T3 T4
+	// / \
+	// T3 T4
+	private Node leftRotate(Node y) {
+
+		Node x = y.right;
+		Node T2 = x.left;
+		x.left = y;
+		y.right = T2;
+
+		// 更新height
+		y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+		x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+
+		return x;
 	}
 
 	public static void main(String[] args) {
